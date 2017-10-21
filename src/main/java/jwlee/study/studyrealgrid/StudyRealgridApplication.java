f2318799764662c54bf740b7ea27fe2d53800748
jwlee <kenlima@wemakeprice.com>
2017-10-22 08:29:03 +0900

    start

---
+++ src/main/java/jwlee/study/studyrealgrid/StudyRealgridApplication.java
@@ -0,0 +1,12 @@
+package jwlee.study.studyrealgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRealgridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyRealgridApplication.class, args);
+	}
+}
---
+++ src/test/java/jwlee/study/studyrealgrid/StudyRealgridApplicationTests.java
@@ -0,0 +1,16 @@
+package jwlee.study.studyrealgrid;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudyRealgridApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
